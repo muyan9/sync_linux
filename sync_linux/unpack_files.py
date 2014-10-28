7c9e9cdce4b29dc84a18696270e2c75ed6ff5ce8
root <root@localhost>
2014-10-28 23:18:45 +0800

    s

--- sync_linux/unpack_files.py
+++ sync_linux/unpack_files.py
@@ -19,43 +19,10 @@ dict_type_package = {'iso':'7z',
                      'lzip':'', 
                      'lzop':''}
 
-# def _mktemp(type):
-#     if not type or type==enum_filetype.other:
-#         cmd = '/bin/mktemp -d --suffix _other'
-#     else:
-#         cmd = '/bin/mktemp -d --suffix _%s' % type
-#     
-#     return os.popen(cmd).read().strip()
-# 
 # def unpack_iso(filename):
 #     dir_tmp = _mktemp(enum_filetype.iso)
 #     os.popen('mount -o loop,ro "%s" %s' % (filename, dir_tmp))
 #     return dir_tmp
-# 
-# def unpack_zip(filename):
-#     dir_tmp = _mktemp(enum_filetype.zip)
-#     os.popen('unzip "%s" -d %s' % (filename, dir_tmp))
-#     return dir_tmp
-# 
-# def unpack_tarbz2(filename):
-#     dir_tmp = _mktemp(enum_filetype.tarbz2)
-#     pass
-# 
-# def unpack_gz(filename):
-#     dir_tmp = _mktemp(enum_filetype.tarbz2)
-#     gf = gzip.open(filename, 'rb')
-# #     data = gf.read()
-#     primaryname = os.path.splitext(os.path.basename(path))[0]
-#     f = open(os.path.join(dir_tmp, primaryname), 'wb')
-#     data = gf.read(1024*1024)
-#     while data:
-#         f.write(data)
-#         data = gf.read(1024*1024)
-#     f.close()
-#     gf.close()
-#     return dir_tmp
-
-
 
 """
   -j, --bzip2                filter the archive through bzip2
@@ -71,29 +38,11 @@ dict_type_package = {'iso':'7z',
 #--recursion
 #-C, --directory=DIR
 class unpack():
-    def is_pack(self, filename):
-        if not os.path.exists(filename):
-            raise IOError("file '%s' not found!" % filename)
-        
-        l_filename = os.path.splitext(filename)
-        if len(l_filename)==2:
-            #splitextf分解/root/a.tar.gz的结果是tuple: ('/root/a.tar', '.gz')
-            #所以要把扩展名的第一个字符’.’去掉
-            extname = l_filename[1][1:]
-            if extname in dict_type_package.keys():
-                return dict_type_package[extname]
-        
-        return None
-    
-    def _mktemp(self):
-        cmd = '/bin/mktemp -d -p /dev/shm/'
-        return os.popen(cmd).read().strip()
-     
     def unpack_7z(self, filename):
         if not os.path.exists(filename):
             raise IOError("file '%s' not found!" % filename)
         
-        dir_tmp = self._mktemp()
+        dir_tmp = mktemp()
         
         cmd = '7za x -y -o%(dir_dst)s %(dir_src)s > /dev/null' % {'dir_src':filename, 'dir_dst':dir_tmp}
         os.popen(cmd)
@@ -102,7 +51,7 @@ class unpack():
     
     #, flag_recursive=True
     def unpack(self, filename):
-        ispack = self.is_pack(filename)
+        ispack = is_pack(filename)
         if ispack:
             #查找是否有对应类型的解压缩方法，若有，直接动态调用对应的方法
             for i in inspect.getmembers(unpack):
@@ -111,6 +60,24 @@ class unpack():
                     return dir_temp
         
         return None
+
+def is_pack(filename):
+    if not os.path.exists(filename):
+        raise IOError("file '%s' not found!" % filename)
+    
+    l_filename = os.path.splitext(filename)
+    if len(l_filename)==2:
+        #splitextf分解/root/a.tar.gz的结果是tuple: ('/root/a.tar', '.gz')
+        #所以要把扩展名的第一个字符’.’去掉
+        extname = l_filename[1][1:]
+        if extname in dict_type_package.keys():
+            return dict_type_package[extname]
+    
+    return None
+
+def mktemp():
+    cmd = '/bin/mktemp -d -p /dev/shm/'
+    return os.popen(cmd).read().strip()
     
 def removefiles(file):
     if os.path.isdir(file):
@@ -127,74 +94,59 @@ def walk_dir(filename):
             
     return list_files
 
-# def do_work(filename, pkgname_parent="", flag_delete=False):
-#     u = unpack()
-#     l = []
-#     for file in walk_dir(filename):
-#         dir_temp = u.unpack(file)
-#         if dir_temp:
-#             d_md5 = hash.md5_file(file)
-#             d_sha1 = hash.sha1_file(file)
-#             d_sha256 = hash.sha256_file(file)
-# #             pkgname_parent = file.partition(filename)[2][1:]
-# #             if pkgname_parent == os.path.basename(file):
-# #                 pkgname_parent = ""
-#             print pkgname_parent
-#             l.append((file, pkgname_parent, d_md5, d_sha1, d_sha256))
-#             ll = do_work(dir_temp, file, flag_delete=True)
-#             l = l + ll
-# #             print file, dir_temp
-#         else:
-# #             print 'hash:', pkgname_parent, file
-#             #TODO: hash it
-# #             print 1
-#             d_md5 = hash.md5_file(file)
-#             d_sha1 = hash.sha1_file(file)
-#             d_sha256 = hash.sha256_file(file)
-#             t_filename = file.partition(filename)[2]
-#             print t_filename
-#             l.append((file, pkgname_parent, d_md5, d_sha1, d_sha256))
-#             removefiles(file)
-#     else:
-#         if flag_delete:
-# #             print 'removefiles:', filename
-#             removefiles(filename)
-#             
-#     return l
 
 o_unpack = unpack()
 
 def do_work(filename, pkgname_parent="", flag_delete=False):
+    '''
+{
+    ('/a.tar.gz', '', 'md5', 'sha1', 'sha256'): 
+    [
+        {
+            ('/a.tar', '/a.tar.gz', 'md5', 'sha1', 'sha256'): 
+            [
+                {
+                    ('/a/b.tar.gz', '/a.tar', 'md5', 'sha1', 'sha256'): 
+                    [
+                        {
+                            ('/b.tar', '/a/b.tar.gz', 'md5', 'sha1', 'sha256'): 
+                            [
+                                ('/b/f4', '/b.tar', 'md5', 'sha1', 'sha256'), 
+                                ('/b/f3', '/b.tar', 'md5', 'sha1', 'sha256')
+                            ]
+                        }
+                    ]
+                }, 
+                ('/a/f2', '/a.tar', 'md5', 'sha1', 'sha256'), 
+                ('/a/f1', '/a.tar', 'md5', 'sha1', 'sha256')
+            ]
+        }
+    ]
+}
+('/t1', '', 'md5', 'sha1', 'sha256')
+
+'''
     l = []
+    d = {}
     for file in walk_dir(filename):
-        if o_unpack.is_pack(file):
+        d_md5 = hash.md5_file(file)
+        d_sha1 = hash.sha1_file(file)
+        d_sha256 = hash.sha256_file(file)
+        filesize = os.stat(file).st_size
+        t_filename = file.partition(filename)[2]
+        
+        if is_pack(file):
             dir_temp = o_unpack.unpack(file)
-            d_md5 = hash.md5_file(file)
-            d_sha1 = hash.sha1_file(file)
-            d_sha256 = hash.sha256_file(file)
-            t_filename = file.partition(filename)[2]
-#             if pkgname_parent == os.path.basename(file):
-#                 pkgname_parent = ""
-            print pkgname_parent
-            l.append((t_filename, pkgname_parent, d_md5, d_sha1, d_sha256))
-            ll = do_work(dir_temp, file, flag_delete=True)
-            l = l + ll
-#             print file, dir_temp
+            pkg_info = (t_filename, pkgname_parent, d_md5, d_sha1, d_sha256, filesize)
+            ll = do_work(dir_temp, t_filename, flag_delete=True)
+            d[pkg_info] = ll
+            l.append(d)
         else:
-#             print 'hash:', pkgname_parent, file
-            #TODO: hash it
-#             print 1
-            d_md5 = hash.md5_file(file)
-            d_sha1 = hash.sha1_file(file)
-            d_sha256 = hash.sha256_file(file)
-            t_filename = file.partition(filename)[2]
-            print t_filename
-            t_pkg_parent = pkgname_parent.partition(filename)[2]
-            l.append((t_filename, t_pkg_parent, d_md5, d_sha1, d_sha256))
-            removefiles(file)
+            l.append((t_filename, pkgname_parent, d_md5, d_sha1, d_sha256))
+            if flag_delete:
+                removefiles(file)
     else:
         if flag_delete:
-#             print 'removefiles:', filename
             removefiles(filename)
             
     return l
@@ -210,7 +162,10 @@ if __name__ == "__main__":
     #delete tmp files and do not remove any origin files
     #TODO: first run, hash all files
     path = "/root/test/unpack/gz"
-    l = do_work(path, flag_delete=False)
+    l = do_work(path, pkgname_parent='centos', flag_delete=False)
     for i in l :
         print i
     #TODO: not first run, read increment list
+    
+    #TODO: 有些文件是更新的，如何处理
+    #TODO: 加入时间字段，区别历史版本
